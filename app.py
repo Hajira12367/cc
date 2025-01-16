9974c2a0f84dc3344461360efa9a82a7f96de7af
Hajira <51800744+Hajira12367@users.noreply.github.com>
2025-01-16 12:04:54 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+sentence = input('Enter a sentence: ')
+print('your sentence is: ' +sentence)
+word1 = input('Enter a word to replace:')
+word2 = input('Enter a word to replace with: ')
+print(sentence.replace(word1, word2))
